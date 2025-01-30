3ebd925c634198a68e4a62ae3862878f0ea4f10f
koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2025-01-30 20:27:18 +0000

    Refactor the membership form model
    So that the way the form model is written is more
    consistent with how it is done elsewhere. Also,
    all validations are now encompassed in the
    form model, instead of spread out between the
    form model and controller

--- spec/models/user_membership_form_spec.rb
+++ spec/models/user_membership_form_spec.rb
@@ -3,65 +3,72 @@ describe UserMembershipForm do
   let(:password) { "S3Cret!123" }
   let(:service_email) { "tom@gov.uk" }
   let(:organisation_name) { "Gov Org 1" }
+  let(:confirmation_token) { user.confirmation_token }
   let(:user) { FactoryBot.create(:user, :unconfirmed, name: "harry", email: "harry@gov.uk") }
-  let(:form) { UserMembershipForm.new(name:, password:, service_email:, organisation_name:) }
+  let(:form) { UserMembershipForm.new(name:, password:, service_email:, organisation_name:, confirmation_token:) }
 
   describe "Updating attributes" do
     it "updates the name" do
-      form.write_to(user)
-      expect(user.name).to eq("tom")
+      form.save!
+      expect(user.reload.name).to eq("tom")
     end
     it "updates the password" do
-      form.write_to(user)
-      expect(user.password).to eq("S3Cret!123")
+      form.save!
+      expect(user.reload.valid_password?(password)).to eq(true)
     end
     it "updates the users organisation name" do
-      form.write_to(user)
-      expect(user.organisations.first.name).to eq("Gov Org 1")
+      form.save!
+      expect(user.reload.organisations.first.name).to eq("Gov Org 1")
     end
     it "updates the users organisation email address" do
-      form.write_to(user)
-      expect(user.organisations.first.service_email).to eq("tom@gov.uk")
+      form.save!
+      expect(user.reload.organisations.first.service_email).to eq("tom@gov.uk")
     end
     it "confirms the user" do
-      expect { form.write_to(user) }.to change { user.confirmed? }.from(false).to(true)
+      expect { form.save! }.to change { user.reload.confirmed? }.from(false).to(true)
     end
     it "adds an organisation" do
-      expect { form.write_to(user) }.to change { user.organisations.count }.from(0).to(1)
+      expect { form.save! }.to change { user.reload.organisations.count }.from(0).to(1)
     end
     it "confirms the membership" do
-      form.write_to(user)
-      expect(user.default_membership.confirmed?).to be true
+      form.save!
+      expect(user.reload.memberships.last.confirmed?).to be true
     end
     it "returns true" do
-      expect(form.write_to(user)).to be true
+      expect(form.save!).to be true
     end
   end
   describe "failing validations" do
+    describe "invalid token" do
+      let(:confirmation_token) { "invalid" }
+      it "raises an error" do
+        expect { form }.to raise_error(UserMembershipForm::InvalidTokenError, /Invalid confirmation token/)
+      end
+    end
     describe "user already confirmed" do
       let(:user) { FactoryBot.create(:user, name: "harry", email: "harry@gov.uk") }
-      it "returns false" do
-        expect(form.write_to(user)).to be false
+      it "raises an error" do
+        expect { form }.to raise_error(UserMembershipForm::AlreadyConfirmedError, /already confirmed/)
       end
     end
     describe "blank name and blank password" do
       let(:name) { "" }
       let(:password) { "" }
       it "copies the validations" do
-        form.write_to(user)
+        form.save!
         expect(form.errors.map(&:full_message)).to match_array(["Name can't be blank",
                                                                 "Password can't be blank",
                                                                 "Password is too short (minimum is 6 characters)"])
       end
       it "returns false" do
-        expect(form.write_to(user)).to be false
+        expect(form.save!).to be false
       end
     end
     describe "blank service email and blank organisation" do
       let(:organisation_name) { "" }
       let(:service_email) { "" }
       it "copies the validations" do
-        form.write_to(user)
+        form.save!
         expect(form.errors.map(&:full_message)).to match_array(["Name can't be blank",
                                                                 "Service email must be in the correct format, like name@example.com",
                                                                 "Name isn't in the organisations allow list"])
