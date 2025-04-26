a3ade41218ef80d9ff8a3fd67aeeec84ae78d010
Iram S Tahraoui <34037863+iram-shehzadi@users.noreply.github.com>
2025-04-26 22:34:30 +0100

    clean up code for sort link and add tests for functionality

--- app/helpers/organisations_helper.rb
+++ app/helpers/organisations_helper.rb
@@ -1,11 +1,10 @@
 module OrganisationsHelper
   def sort_link(column, title = nil)
     title ||= column.titleize
-    css_class = column == sort_column ? "current #{sort_direction}" : nil
     default_sort_direction = column == "name" ? "asc" : "desc"
     is_active_sort_column = column == sort_column
     direction = is_active_sort_column ? flip_sort_direction(sort_direction) : default_sort_direction
-    link_to title, { sort: column, direction: }, class: css_class
+    link_to title, { sort: column, direction: }
   end
 
   def flip_sort_direction(direction)
