a74dc22e04f18b2a319e3a72668e75dcaec0a1e0
Ethan Cane <ethan.cane@digital.cabinet-office.gov.uk>
2026-06-26 19:53:22 +0100

    Refactored use case to be more composable.

--- lib/use_cases/administrator/publish_organisation_count.rb
+++ lib/use_cases/administrator/publish_organisation_count.rb
@@ -3,22 +3,39 @@ require "logger"
 module UseCases
   module Administrator
     class PublishOrganisationCount
+      def initialize(logger: Logger.new($stdout))
+        @logger = logger
+      end
+
       def publish
-        logger = Logger.new($stdout)
-        count = ::Organisation.count(:name)
-        metric = {
+        send_to_s3
+        send_to_api
+      end
+
+    private
+
+      def metric
+        ::Organisation.count(:name)
+      end
+
+      def stats
+        {
           metric_name: "organisation-count",
-          count: count,
+          count: metric,
           run_time: Time.zone.today,
         }
+      end
 
-        logger.info("BEGIN: Writing to S3 bucket...")
-        Gateways::S3.new(**Gateways::S3::S3_METRICS_BUCKET).write(metric.to_yaml)
-        logger.info("END: Writing to S3 bucket")
+      def send_to_s3
+        @logger.info("BEGIN: Writing to S3 bucket...")
+        Gateways::S3.new(**Gateways::S3::S3_METRICS_BUCKET).write(stats.to_yaml)
+        @logger.info("END: Writing to S3 bucket - (Org count: #{stats[:count]})")
+      end
 
-        logger.info("BEGIN: Posting to metrics API...")
-        UseCases::PerformancePlatform::MetricsApiPublisher.publish(metric)
-        logger.info("END: Posting to metrics API")
+      def send_to_api
+        @logger.info("BEGIN: Posting to metrics API...")
+        UseCases::PerformancePlatform::MetricsApiPublisher.publish(stats)
+        @logger.info("END: Posting to metrics API - (Org count: #{stats[:count]})")
       end
     end
   end
